8bfd9d4b7719270d7575ac8754c766349ebbfb4b
mellowmark27 <msouthgate@protonmail.com>
2026-02-26 14:09:02 +0000

    Create request.py

---
+++ request.py
@@ -0,0 +1,7 @@
+headers = {
+    'User-Agent': 'Mozilla/5.0 ...',
+    'Referer': 'https://www.google.com/',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+}
+response = requests.get(url, headers=headers)
